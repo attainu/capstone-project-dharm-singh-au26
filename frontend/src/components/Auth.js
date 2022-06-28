e9acbddb6471be25a8d8f1b88c827654e25d04a4
dharm <dharmjat45@gmail.com>
2022-06-28 18:55:38 +0530

    auth file completed

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,17 +1,81 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [isSignup, setIsSignup] = useState(false);
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
   return (
     <div>
-      <form>
-        <Box>
-          <Typography>Login</Typography>
-          <TextField />
-          <TextField />
-          <TextField />
-          <Button>Submit</Button>
-          <Button>Change To SignUp</Button>
+      <form onSubmit={handleSubmit}>
+        <Box
+          maxWidth={400}
+          display="flex"
+          flexDirection={"column"}
+          alignItems="center"
+          justifyContent={"center"}
+          boxShadow="10px 10px 20px #ccc"
+          padding={3}
+          margin="auto"
+          marginTop={5}
+          borderRadius={5}
+        >
+          <Typography variant="h2" padding={3} textAlign="center">
+            {isSignup ? "Signup" : "Login"}
+          </Typography>
+
+          {isSignup && (
+            <TextField
+              name="name"
+              onChange={handleChange}
+              value={inputs.name}
+              placeholder="Name"
+              margin="normal"
+            />
+          )}
+          <TextField
+            name="email"
+            onChange={handleChange}
+            value={inputs.email}
+            type={"email"}
+            placeholder="E-Mail"
+            margin="normal"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            type={"password"}
+            value={inputs.password}
+            placeholder="Password"
+            margin="normal"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ borderRadius: 3, marginTop: 3 }}
+            color="warning"
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={() => setIsSignup(!isSignup)}
+            sx={{ borderRadius: 3, marginTop: 3 }}
+          >
+            Change To {isSignup ? "Login" : "Signup"}
+          </Button>
         </Box>
       </form>
     </div>
